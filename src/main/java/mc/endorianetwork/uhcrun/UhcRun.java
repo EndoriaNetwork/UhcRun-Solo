977c1435b5ac2ffd1ddbae8c820561d130642860
hugob147 <huderrien@laposte.net>
2018-10-21 16:13:19 +0200

    initial commit

---
+++ src/main/java/mc/endorianetwork/uhcrun/UhcRun.java
@@ -0,0 +1,16 @@
+package mc.endorianetwork.uhcrun;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UhcRun extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
