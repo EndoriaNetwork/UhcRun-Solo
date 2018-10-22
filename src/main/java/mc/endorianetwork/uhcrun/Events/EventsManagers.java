ff75a2f51f2c415bff02a1cbaa0f277a05c69341
hugob147 <huderrien@laposte.net>
2018-10-22 19:53:55 +0200

    Update

--- src/main/java/mc/endorianetwork/uhcrun/Events/EventsManagers.java
+++ src/main/java/mc/endorianetwork/uhcrun/Events/EventsManagers.java
@@ -1,6 +1,9 @@
 package mc.endorianetwork.uhcrun.Events;
 
 import mc.endorianetwork.uhcrun.UhcRun;
+import mc.endorianetwork.uhcrun.Utils.UHCBlocksDrops;
+import mc.endorianetwork.uhcrun.Utils.UHCSkullRegen;
+import mc.endorianetwork.uhcrun.Utils.UHCSpeedRecipes;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 
@@ -10,6 +13,9 @@ public class EventsManagers {
         PluginManager pm = Bukkit.getPluginManager();
         pm.registerEvents(new UHCJoin(), pl);
         pm.registerEvents(new UHCNoFall(), pl);
+        pm.registerEvents(new UHCSpeedRecipes(), pl);
+        pm.registerEvents(new UHCSkullRegen(), pl);
+        pm.registerEvents(new UHCBlocksDrops(), pl);
 
     }
 }
